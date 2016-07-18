ef4c9ce2876ca34ee76b38ac2f99144c16d01824
WingBot <zerongsu@163.com>
2016-07-18 17:25:29 +0800

    add paramter of the AGV

---
+++ include/hans_bringup/AgvPara.h
@@ -0,0 +1,16 @@
+/*
+ * AgvPara.h
+ *
+ *  Created on: Jul 18, 2016
+ *      Author: agv
+ */
+
+#ifndef INCLUDE_HANS_BRINGUP_AGVPARA_H_
+#define INCLUDE_HANS_BRINGUP_AGVPARA_H_
+
+const float WheelR = 0.0675;	//WheelRadius
+const float WheelBase= 0.589;	//WheelBase
+
+
+
+#endif /* INCLUDE_HANS_BRINGUP_AGVPARA_H_ */
